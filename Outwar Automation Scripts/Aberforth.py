22aa3e61159f604594bdb572a58115b5bd87b844
Jbuiles <35709632+Jbuiles@users.noreply.github.com>
2018-06-05 21:33:05 -0400

    Update Aberforth.py

--- Outwar Automation Scripts/Aberforth.py
+++ Outwar Automation Scripts/Aberforth.py
@@ -22,18 +22,18 @@ def init_driver():
     return browser
 
 def login():
-	browser.get('https://+yourserver+.outwar.com/login')
+	browser.get('https://'+yourserver+'.outwar.com/login')
 	user = browser.find_element_by_name('login_username')
 	password = browser.find_element_by_name('login_password')
-	user.send_keys('yourlogin')
-	password.send_keys('yourpassword')
+	user.send_keys(yourlogin)
+	password.send_keys(yourpassword)
 	submit = browser.find_element_by_name('submitit')
 	submit.submit()
 	print('Successful Login')
 	time.sleep(2)
 	
 def formraid():
-	browser.get('https://+yourserver+.outwar.com/world?suid=')#Your Raid Former
+	browser.get('https://'+yourserver+'.outwar.com/world?suid=')#Your Raid Former
 	try:
 		button1 = browser.wait.until(EC.presence_of_element_located(
                     (By.CSS_SELECTOR, '#roomDetails > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3) > a:nth-child(1) > img:nth-child(1)')))
@@ -64,7 +64,7 @@ def joinraid():
         print('Successful Join')
 
 def launchraid():
-	browser.get('https://+yourserver+.outwar.com/world?suid=')#Your Raid Former
+	browser.get('https://'+yourserver+'.outwar.com/world?suid=')#Your Raid Former
 	try:
 		raid = browser.wait.until(EC.presence_of_element_located(
                     (By.CSS_SELECTOR, '#roomDetails > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(3) > a:nth-child(1) > img:nth-child(1)')))
